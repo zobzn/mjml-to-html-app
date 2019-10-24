b769b5f64eae01f704c166fbc92284cdf44f04e9
zema <zlobzn@gmail.com>
2019-10-24 10:15:52 +0300

    use images from unsplash; refactored Iframe element

--- components/Iframe.js
+++ components/Iframe.js
@@ -1,75 +1,11 @@
-// https://github.com/ryanseddon/react-frame-component
+import React from "react";
 
-import React, { Component } from "react";
-
-export default class Iframe extends Component {
-  constructor(props, context) {
-    super(props, context);
-  }
-
-  componentDidMount() {
-    const doc = this.getDoc();
-
-    if (doc) {
-      console.log("componentDidMount - " + doc.readyState);
-
-      if (doc.readyState === "complete") {
-        this.handleLoad();
-        // this.forceUpdate();
-      } else {
-        this.node.addEventListener("load", this.handleLoad);
-      }
-    } else {
-      console.log("componentDidMount - no doc");
-    }
-  }
-
-  componentDidUpdate(oldProps) {
-    console.log("componentDidUpdate");
-
-    this.writeHTML();
-  }
-
-  componentWillUnmount() {
-    console.log("componentWillUnmount");
-
-    this.node.removeEventListener("load", this.handleLoad);
-  }
-
-  getDoc() {
-    return this.node ? this.node.contentDocument : null;
-  }
-
-  handleLoad = () => {
-    console.log("handleLoad");
-
-    // this.forceUpdate();
-    this.writeHTML();
-  };
-
-  writeHTML() {
-    let doc = this.getDoc();
-    if (doc) {
-      console.log("writeHTML 1");
-      doc.open();
-      doc.write(this.props.html);
-      doc.close();
-    } else {
-      console.log("writeHTML 0");
-    }
-  }
-
-  render() {
-    return (
-      <iframe
-        src="about:blank"
-        scrolling="yes"
-        frameBorder="0"
-        style={{ width: "100%", height: "100%" }}
-        ref={node => {
-          this.node = node;
-        }}
-      ></iframe>
-    );
-  }
-}
+export default ({ html }) => (
+  <iframe
+    style={{ width: "100%", height: "100%" }}
+    src="about:blank"
+    scrolling="yes"
+    frameBorder="0"
+    srcDoc={html}
+  ></iframe>
+);
--- pages/index.js
+++ pages/index.js
@@ -17,77 +17,77 @@ import mjml from "mjml";
 import Iframe from "../components/Iframe";
 
 const initialSrc = `<mjml>
-<mj-body>
-  <mj-section background-color="#f0f0f0">
-    <mj-column>
-      <mj-text font-style="italic" font-size="20px" color="#626262">
-        My Company
-      </mj-text>
-    </mj-column>
-  </mj-section>
+  <mj-body>
+    <mj-section background-color="#f0f0f0">
+      <mj-column>
+        <mj-text font-style="italic" font-size="20px" color="#626262">
+          My Company
+        </mj-text>
+      </mj-column>
+    </mj-section>
 
-  <mj-section background-url="//1.bp.blogspot.com/-TPrfhxbYpDY/Uh3Refzk02I/AAAAAAAALw8/5sUJ0UUGYuw/s1600/New+York+in+The+1960's+-+70's+(2).jpg" background-size="cover" background-repeat="no-repeat">
-    <mj-column>
-      <mj-text align="center" color="#fff" font-size="40px" font-family="Helvetica Neue">Slogan here</mj-text>
-      <mj-button background-color="#F63A4D" href="#">
-        Promotion
-      </mj-button>
-    </mj-column>
-  </mj-section>
+    <mj-section background-url="https://picsum.photos/seed/header/900/340" background-size="cover" background-repeat="no-repeat">
+      <mj-column>
+        <mj-text align="center" color="#fff" font-size="40px" font-family="Helvetica Neue">Slogan here</mj-text>
+        <mj-button background-color="#F63A4D" href="#">
+          Promotion
+        </mj-button>
+      </mj-column>
+    </mj-section>
 
-  <mj-section background-color="#fafafa">
-    <mj-column width="400px">
-      <mj-text font-style="italic" font-size="20px" font-family="Helvetica Neue" color="#626262">My Awesome Text</mj-text>
-      <mj-text color="#525252">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin rutrum enim eget magna efficitur, eu semper augue semper. Aliquam erat volutpat. Cras id dui lectus. Vestibulum sed finibus lectus, sit amet suscipit nibh. Proin nec commodo purus. Sed eget
-        nulla elit. Nulla aliquet mollis faucibus.
-      </mj-text>
-      <mj-button background-color="#F45E43" href="#">Learn more</mj-button>
-    </mj-column>
-  </mj-section>
+    <mj-section background-color="#fafafa">
+      <mj-column width="400px">
+        <mj-text font-style="italic" font-size="20px" font-family="Helvetica Neue" color="#626262">My Awesome Text</mj-text>
+        <mj-text color="#525252">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin rutrum enim eget magna efficitur, eu semper augue semper. Aliquam erat volutpat. Cras id dui lectus. Vestibulum sed finibus lectus, sit amet suscipit nibh. Proin nec commodo purus. Sed eget
+          nulla elit. Nulla aliquet mollis faucibus.
+        </mj-text>
+        <mj-button background-color="#F45E43" href="#">Learn more</mj-button>
+      </mj-column>
+    </mj-section>
 
-  <mj-section background-color="white">
-    <mj-column>
-      <mj-image width="200px" src="https://designspell.files.wordpress.com/2012/01/sciolino-paris-bw.jpg" />
-    </mj-column>
-    <mj-column>
-      <mj-text font-style="italic" font-size="20px" font-family="Helvetica Neue" color="#626262">
-        Find amazing places
-      </mj-text>
-      <mj-text color="#525252">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin rutrum enim eget magna efficitur, eu semper augue semper. Aliquam erat volutpat. Cras id dui lectus. Vestibulum sed finibus lectus.</mj-text>
-    </mj-column>
-  </mj-section>
+    <mj-section background-color="white">
+      <mj-column>
+        <mj-image width="200px" src="https://picsum.photos/seed/content-image/600/400" />
+      </mj-column>
+      <mj-column>
+        <mj-text font-style="italic" font-size="20px" font-family="Helvetica Neue" color="#626262">
+          Find amazing places
+        </mj-text>
+        <mj-text color="#525252">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin rutrum enim eget magna efficitur, eu semper augue semper. Aliquam erat volutpat. Cras id dui lectus. Vestibulum sed finibus lectus.</mj-text>
+      </mj-column>
+    </mj-section>
 
-  <mj-section background-color="#fbfbfb">
-    <mj-column>
-      <mj-image width="100px" src="//191n.mj.am/img/191n/3s/x0l.png" />
-    </mj-column>
-    <mj-column>
-      <mj-image width="100px" src="//191n.mj.am/img/191n/3s/x01.png" />
-    </mj-column>
-    <mj-column>
-      <mj-image width="100px" src="//191n.mj.am/img/191n/3s/x0s.png" />
-    </mj-column>
-  </mj-section>
+    <mj-section background-color="#fbfbfb">
+      <mj-column>
+        <mj-image width="100px" src="https://picsum.photos/seed/content-image-1/100/100" />
+      </mj-column>
+      <mj-column>
+        <mj-image width="100px" src="https://picsum.photos/seed/content-image-2/100/100" />
+      </mj-column>
+      <mj-column>
+        <mj-image width="100px" src="https://picsum.photos/seed/content-image-3/100/100" />
+      </mj-column>
+    </mj-section>
 
-  <mj-section background-color="#e7e7e7">
-    <mj-column>
-      <mj-button href="#">Hello There!</mj-button>
-      <mj-social font-size="15px" icon-size="30px" mode="horizontal">
-        <mj-social-element name="facebook" href="https://mjml.io/">
-          Facebook
-        </mj-social-element>
-        <mj-social-element name="google" href="https://mjml.io/">
-          Google
-        </mj-social-element>
-        <mj-social-element name="twitter" href="https://mjml.io/">
-          Twitter
-        </mj-social-element>
-      </mj-social>
-    </mj-column>
-  </mj-section>
-</mj-body>
+    <mj-section background-color="#e7e7e7">
+      <mj-column>
+        <mj-button href="#">Hello There!</mj-button>
+        <mj-social font-size="15px" icon-size="30px" mode="horizontal">
+          <mj-social-element name="facebook" href="https://mjml.io/">
+            Facebook
+          </mj-social-element>
+          <mj-social-element name="google" href="https://mjml.io/">
+            Google
+          </mj-social-element>
+          <mj-social-element name="twitter" href="https://mjml.io/">
+            Twitter
+          </mj-social-element>
+        </mj-social>
+      </mj-column>
+    </mj-section>
+  </mj-body>
 </mjml>`;
 
 const mjml2html = src => {
